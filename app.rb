27db35c60f03e455c260096ea932d1deed58f60c
Connie Hyman <constance.hyman@mchsi.com>
2018-11-19 01:48:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
 
-    if operation == "add"
+    if operation == add
       @solution = number1 + number2
       "#{@solution}"
     elsif operation == "multiply"
