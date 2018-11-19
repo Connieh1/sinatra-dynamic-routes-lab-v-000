9aa7b91918a517fb849a3426c9197e06e0e9381d
Connie Hyman <constance.hyman@mchsi.com>
2018-11-19 01:34:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,4 +21,9 @@ class App < Sinatra::Base
     @phrase = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
     "#{@phrase}"
   end
+
+  get '/:operation/:number1/:number2' do
+    @solution = params[:number1].to_i params[:operation] params[:number2].to_i
+    "#{@solution}"
+  end  
 end
