36eca87ecd6efb2145e0c64fb0f44d262759f3cc
Connie Hyman <constance.hyman@mchsi.com>
2018-11-19 01:51:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -33,13 +33,13 @@ class App < Sinatra::Base
     elsif operation == "subtract"
       @solution = number1 - number2
       "#{@solution}"
-    elsif operation == "multiply"
-      @solution = number1 * number2
-      "#{@solution}"
+    # elsif operation == "multiply"
+    #   @solution = number1 * number2
+    #   "#{@solution}"
     # elsif operation == "divide"
     #   @solution = number1 / number2
     #   "#{@solution}"
-    
+
     # else
     #   "Cannot be evaluated."
     end
